0facea21f4cdbbf092388727fda445df7900f359
kidist311 <alemayehukidist09@gmail.com>
2025-09-08 14:51:07 +0300

    added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
+import { ChevronRight } from "lucide-react";
+
 export default function Home() {
     return (
-      <div className="relative w-full max-w-[1300px] h-[1800px] bg-gray-300  ">
+      <div className="relative w-full max-w-[1300px] h-[5000px] bg-gray-300  ">
         <div className="  top-0 left-0 w-full max-w-[1300px] h-[1000px] md:h-[570px] bg-yellow-200 relative ">
         
         <div className="w-full  h-[600px] md:h-[570px]  bg-gray-50 md:w-full ">
@@ -35,6 +37,8 @@ export default function Home() {
         <div className=" absolute bottom-0 md:top-0  md:right-0 w-full h-[480px] md:h-[570px]  bg-orange-500  md:w-[20px]"></div>
         </div>
 
+        {/* payment section */}
+        
         <div className="  top-0 left-0 w-full max-w-[1300px] h-[300px]  bg-white relative ">
             <p className="flex justify-center font-bold text-2xl py-32 ">Order from your favorite app today!</p>
             <div className="flex justify-center gap-3 md:gap-10 flex-wrap bg-white items-center -mt-20">
@@ -44,7 +48,67 @@ export default function Home() {
                 <a className="" href="https://www.rappi.com.mx/"><img src="/icons/Rappi.svg" alt="Uber Eats" className="w-35 h-35" /></a>
                 <a className="" href="https://www.grubhub.com/"><img src="/icons/guhu.svg" alt="Uber Eats" className="w-35 h-35" /></a>
             </div>
-            <hr className="mr-5 ml-5 border-red-600" />
+            <hr className="mr-5 ml-5 mt-15 border-gray-500" />
+        </div>
+
+        {/* Browse our menu */}
+        <div className="  w-full max-w-[1300px] h-[2400px] md:h-[1000px]  bg-white  ">
+          <p className="flex justify-center font-bold text-2xl py-96 md:py-72 md:mb-15 ">Browse our menu</p>
+              <div className="flex flex-col md:flex-row justify-center items-center -mt-80 gap-5 md:p-5">
+                    <div className="w-[500px] h-[400px] border rounded-2xl border-gray-300 items-center flex flex-col justify-evenly p-3
+                        transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+                        
+                        <img src="/icons/post.svg" alt="Uber Eats" className="w-30 h-30   rounded-full " />
+                        <h3 className="font-bold text-2xl -mt-4">Breakfast</h3>
+                        <p className="text-gray-500 -mt-4 text-center">Lorem ipsum dolor sit amet consectetur adipiscing </p>
+                        <button className="text-orange-500 font-bold flex items-center gap-1 hover:gap-2 transition-all duration-300">
+                          Explore menu <ChevronRight size={18} />
+                        </button>
+                    </div>
+                    <div className="w-[500px] h-[400px] border rounded-2xl border-gray-300 items-center flex flex-col justify-evenly p-3
+                        transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+                        
+                        <img src="/icons/post.svg" alt="Uber Eats" className="w-30 h-30  rounded-full " />
+                        <h3 className="font-bold text-2xl -mt-4">Main Dishes</h3>
+                        <p className="text-gray-500 -mt-4 text-center">Lorem ipsum dolor sit amet consectetur adipiscing </p>
+                        <button className="text-orange-500 font-bold flex items-center gap-1 hover:gap-2 transition-all duration-300">
+                          Explore menu <ChevronRight size={18} />
+                        </button>
+                    </div>
+                    <div className="w-[500px] h-[400px] border rounded-2xl border-gray-300 items-center flex flex-col justify-evenly p-3
+                        transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+                        
+                        <img src="/icons/post.svg" alt="Uber Eats" className="w-30 h-30 rounded-full " />
+                        <h3 className="font-bold text-2xl -mt-4">Drinks</h3>
+                        <p className="text-gray-500 -mt-4 text-center">Lorem ipsum dolor sit amet consectetur adipiscing </p>
+                        <button className="text-orange-500 font-bold flex items-center gap-1 hover:gap-2 transition-all duration-300">
+                          Explore menu <ChevronRight size={18} />
+                        </button>
+                    </div>
+                    <div className="w-[500px] h-[400px] border rounded-2xl border-gray-300 items-center flex flex-col justify-evenly p-3
+                        transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+                        
+                        <img src="/icons/post.svg" alt="Uber Eats" className="w-30 h-30   rounded-full " />
+                        <h3 className="font-bold text-2xl -mt-4">Desserts</h3>
+                        <p className="text-gray-500 -mt-4 text-center">Lorem ipsum dolor sit amet consectetur adipiscing </p>
+                        <button className="text-orange-500 font-bold flex items-center gap-1 hover:gap-2 transition-all duration-300">
+                          Explore menu <ChevronRight size={18} />
+                        </button>
+                    </div>
+                    
+              </div>
+          <div>
+
+          </div>
+          <div className="flex gap-4  py-10 justify-center">
+                        <button className="h-15 px-10 text-xl text-white border rounded-full bg-orange-500 hover:scale-105 hover:shadow-xl transition-transform duration-200 whitespace-nowrap">
+                           Order Online 
+                        </button>
+                        <button className="h-14 px-8 text-xl rounded-full text-gray-900 bg-white border border-gray-300 hover:bg-orange-500 hover:text-white transition-colors duration-300 whitespace-nowrap">
+                        Book Table
+                        </button>
+                   </div>
+
         </div>
         
 
