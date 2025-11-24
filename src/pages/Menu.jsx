e3259b5f8f717187f69a0d68d8d4e5f7102285ca
kidist311 <alemayehukidist09@gmail.com>
2025-11-24 19:53:35 +0300

    Fix import path for Card.jsx

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,6 +1,6 @@
-import { Car } from "lucide-react";
+//import { Car } from "lucide-react";
 import { Link } from "react-router-dom";
-import Card from "../components/card.jsx";
+import Card from "../components/Card.jsx";
 import foods from "../foods.js";
 import Order from "../components/Order.jsx";
 export default function Menu() {
